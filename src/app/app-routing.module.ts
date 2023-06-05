9f2a8af7a87974105a98955fd32ead31a9649419
EdgarInfantes <21100749@ue.edu.pe>
2023-06-04 23:16:16 -0500

    Se agrega el contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { Router } from '@angular/router';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path:'home', component: HomeComponent}
+  {path:'home', component: HomeComponent},
+  {path: 'busqueda', component: BusquedaComponent  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule{
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { Router } from '@angular/router';
 // Imports de Material Angular
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
@@ -12,10 +12,14 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import { HomeComponent } from './home/home.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BusquedaComponent
   ],
   imports: [
     BrowserModule,
@@ -26,9 +30,34 @@ import { HomeComponent } from './home/home.component';
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements OnInit{
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  currentRouteIsContact(): boolean {
+    return this.router.url === '/contacto';
+  }
+  defaultMessage: string = "Hola. Necesito tu ayuda.";
+  chatInput: string = '';
+
+  openChat() {
+    // Abre el chat o realiza cualquier acción necesaria para mostrar el chat
+
+    // Asigna el mensaje predeterminado al campo de entrada del chat
+    this.chatInput = this.defaultMessage;
+
+    // O emite automáticamente el mensaje predeterminado
+    this.sendMessage(this.defaultMessage);
+  }
+
+  sendMessage(message: string) {
+    // Lógica para enviar el mensaje al chat
+  }
+ }
